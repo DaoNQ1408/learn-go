6173610f915f7828406ea9fe38efbea56641d0dd
DaoNQ1408 <nguyenquangdao1408@gmail.com>
2026-04-03 00:48:43 +0700

    learn(go): panic and recover

--- udemo-go-2/6defer/panik/main.go
+++ udemo-go-2/6defer/panik/main.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
+func myPanic(shouldPanic bool) {
+	if shouldPanic {
+		panic("something bad happened in myPanic")
+	}
+
+	fmt.Println("this function will not panic")
+}
+
+func recoverable() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("recovered from panic:", r)
+		}
+	}()
+
+	myPanic(true)
+}
+
 func main() {
-	panic("something bad happened")
+	recoverable()
 }
